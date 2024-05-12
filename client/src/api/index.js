f711461a35dce67840ae8d0c324fec9c8a1d655d
Bob Shens <kiranmanikonda123@gmail.com>
2024-05-12 11:38:45 -0700

    Added Fetch to Update Client State After Post Update/Delete

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -17,10 +17,10 @@ API.interceptors.request.use((req) => {
 // posts-related requests
 
 export const fetchPosts = () => API.get('/posts');
-export const createPost = (newPost) => API.post('/posts', newPost);
+export const createPost = (newPost) => API.post('/posts', newPost).then(fetchPosts());
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
-export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
-export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost).then(fetchPosts());
+export const deletePost = (id) => API.delete(`/posts/${id}`).then(fetchPosts());
 
 
 // auth-related requests
